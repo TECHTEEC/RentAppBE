21009c547005d923ba0c2412dec7347a616dd01e
Eng.Mohammad <Eng.Mohammad@MHDISM>
2025-06-17 13:40:27 +0300

    add main library

---
+++ Shared/GeneralResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace RentAppBE.Shared
+{
+    public class GeneralResponse<T>
+    {
+        public bool Success { get; set; } = true;
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+        public int StatusCode { get; set; }
+
+    }
+}
