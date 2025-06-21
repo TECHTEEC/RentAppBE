701eea385498653f91f12de1ee96d4ed702cfdb2
Eng.Mohammad <Eng.Mohammad@MHDISM>
2025-06-21 12:58:36 +0300

    Edit Auth Model

---
+++ Helper/Enums/VerifiedStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace RentAppBE.Helper.Enums
+{
+    public enum VerifiedStatus
+    {
+        Pending, Reject, Trust
+    }
+}
--- Repositories/TokenService/Dtos/TokenResultDto.cs
+++ Repositories/TokenService/Dtos/TokenResultDto.cs
@@ -1,4 +1,6 @@
-﻿namespace RentAppBE.Repositories.TokenService.Dtos
+﻿using RentAppBE.Helper.Enums;
+
+namespace RentAppBE.Repositories.TokenService.Dtos
 {
     public class TokenResultDto
     {
@@ -6,5 +8,12 @@
         public int AccessTokenExpiresIn { get; set; } // in seconds
         public string RefreshToken { get; set; } = default!;
         public int RefreshTokenExpiresIn { get; set; } // in seconds
+        public string? UserId { get; set; } = null!;
+        public string? UserName { get; set; } = null;
+        public string? Email { get; set; } = null;
+        public string? PhoneNumber { get; set; } = null;
+        public bool IsVendor { get; set; }
+        public VerifiedStatus? VerifiedStatus { get; set; } = null;
+        public bool? IsNewUser { get; set; }
     }
 }
